c0c3b7679eb073d088b8237cea868c7c7dc73029
renegadevi <philip.andersen@codeofmagi.net>
2024-01-14 17:43:35 +0100

    Updating background for minimal-layout

--- layouts/fullscreen.vue
+++ layouts/fullscreen.vue
@@ -7,7 +7,7 @@ useLocaleHead({
 });
 </script>
 <template>
-  <div class="p-6">
+  <div class="min-h-screen bg-slate-100 text-slate-900 dark:bg-slate-900 dark:text-white p-6">
     <div class="rounded-2xl bg-white p-6 shadow-xl dark:bg-slate-800">
       <button
         class="m-2 rounded-lg border border-slate-200 bg-gray-50 p-4 py-3 text-gray-700 hover:cursor-pointer hover:border-slate-200 hover:bg-slate-200 dark:border-gray-700 dark:bg-slate-800 dark:text-white dark:hover:bg-slate-700"
