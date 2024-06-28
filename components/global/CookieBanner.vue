630dcdf79894ec7416d46a27a73117709b78c5d1
renegadevi <philip.andersen@codeofmagi.net>
2024-06-28 22:26:09 +0200

    Added ts to the script tags

--- components/global/CookieBanner.vue
+++ components/global/CookieBanner.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 const { locale } = useI18n();
 /* Example to watch for a cookie being accepted */
 /*
--- components/global/LanguageSelector.vue
+++ components/global/LanguageSelector.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 const { locale } = useI18n();
 
 // update local storage upon language change
